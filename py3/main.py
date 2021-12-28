c98673cc985de70f3da5d0a1d61bd8de9a5d9aa8
hjh <1820458141@qq.com>
2021-12-28 14:44:51 +0800

    hjh2提交1

--- py3/main.py
+++ py3/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('hjh2')
+    print_hi('hjh2Hhhhhhhhhhh')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
